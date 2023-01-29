f4f4aded2fb46d1e0916b5c6e05617b3e98d5826
K1LLsounD <sgol99@mail.ru>
2023-01-29 16:43:25 +0300

    add editing locators

--- tests/test_desktop_and_mobile.py
+++ tests/test_desktop_and_mobile.py
@@ -1,3 +1,4 @@
+from selene.support import by
 from selene.support.conditions import have
 from selene.support.shared import browser
 
@@ -10,9 +11,6 @@ def test_open_desktop_browser(open_desktop_window_size):
 
 def test_open_mobile_browser(open_mobile_window_size):
     browser.open('https://github.com/')
-    browser.element(
-        '[href="/signup?ref_cta=Sign+up&ref_loc=header+logged+out&ref_page=%2F&source=header-home"]').click()
-    browser.element(
-        '[href="/login?return_to=https%3A%2F%2Fgithub.com%2Fsignup%3Fref_cta%3DSign%2Bup%26ref_loc%3Dheader%2Blogged%2Bout%26ref_page%3D%252F%26source%3Dheader-home"]'
-    ).click()
+    browser.element('.Button-content .Button-label').click()
+    browser.element(by.partial_text('Sign in')).click()
     assert browser.element('#login').should(have.text('Sign in to GitHub'))
--- tests/test_parametrize.py
+++ tests/test_parametrize.py
@@ -1,6 +1,7 @@
 # 2. Переопределите параметр с помощью indirect;
 
 import pytest
+from selene.support import by
 from selene.support.conditions import have
 from selene.support.shared import browser
 
@@ -15,9 +16,6 @@ def test_github_desktop(browser_window_size):
 @pytest.mark.parametrize('browser_window_size', ['Mobile'], indirect=True)
 def test_github_mobile(browser_window_size):
     browser.open('https://github.com/')
-    browser.element(
-        '[href="/signup?ref_cta=Sign+up&ref_loc=header+logged+out&ref_page=%2F&source=header-home"]').click()
-    browser.element(
-        '[href="/login?return_to=https%3A%2F%2Fgithub.com%2Fsignup%3Fref_cta%3DSign%2Bup%26ref_loc%3Dheader%2Blogged%2Bout%26ref_page%3D%252F%26source%3Dheader-home"]'
-    ).click()
+    browser.element('.Button-content .Button-label').click()
+    browser.element(by.partial_text('Sign in')).click()
     assert browser.element('#login').should(have.text('Sign in to GitHub'))
